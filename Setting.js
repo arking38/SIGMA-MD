a217aac2040ec152b216d4763629468d63d5c7a4
arking38 <167618146+arking38@users.noreply.github.com>
2024-05-31 10:33:39 +0500

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -75,7 +75,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/03e49e6
 module.exports = {
 
   //==============================[COMPULSORY]=====================================
-  sessionName: process.env.SESSION_ID || '31_05_2024_SIGMA_MD_dTP7tqky_MAHER_ZUBAIR',              // PUT YOUR SESSION ID
+  sessionName: process.env.SESSION_ID || '31_05_2024_SIGMA_MD_aHh1J4jC_MAHER_ZUBAIR',              // PUT YOUR SESSION ID
   botname: process.env.BOT_NAME       || 'sɪɢᴍᴀ-ᴍᴅ',         // ADD YOUR BOT NAME
   ownername:  process.env.OWNER_NAME  || `Arbab Tech`, // ADD YOUR NAME
   HANDLERS: process.env.PREFIX        || '.',              // ADD YOUR BOT PREFIX
